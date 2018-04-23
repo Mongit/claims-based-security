fc50b246f2e048d263cab3d530deb6344a446a85
Mongit <monserrat.jimenez2@gmail.com>
2018-04-23 12:44:36 -0500

    Login using claim-based security

---
+++ AuthenticationTest/AuthenticationTest/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AuthenticationTest.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace AuthenticationTest.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Home/Login
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Login Page.";
+
+            return View();  
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginUser(string returnUrl, string username, string password)
+        {
+            if (username == "Jon" && password == "jon")
+            {
+                var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "jon", ClaimValueTypes.String, "http://localhost:50226/")
+                    };
+                var userIdentity = new ClaimsIdentity(claims, "SecureLogin");
+                var userPrincipal = new ClaimsPrincipal(userIdentity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    userPrincipal,
+                    new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(20),
+                        IsPersistent = false,
+                        AllowRefresh = false
+                    });
+
+                return GoToReturnUrl(returnUrl);
+            }
+            return RedirectToAction("Login","Home");
+        }
+        private IActionResult GoToReturnUrl(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
