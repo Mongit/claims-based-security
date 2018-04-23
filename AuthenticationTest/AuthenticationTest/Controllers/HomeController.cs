633b4f9be4988e20b6961bc6f3eebff291191933
Mongit <monserrat.jimenez2@gmail.com>
2018-04-23 15:33:00 -0500

    Public access only to Login

--- AuthenticationTest/AuthenticationTest/Controllers/HomeController.cs
+++ AuthenticationTest/AuthenticationTest/Controllers/HomeController.cs
@@ -12,24 +12,21 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace AuthenticationTest.Controllers
 {
-    
+    [Authorize]
     public class HomeController : Controller
     {
-
         public IActionResult Index()
         {
             return View();
         }
-
-        [Authorize]
+        
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
-
-        [Authorize]
+        
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -37,7 +34,6 @@ namespace AuthenticationTest.Controllers
             return View();
         }
 
-        [Authorize]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
